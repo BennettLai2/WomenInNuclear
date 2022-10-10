5b39286a0cf6d69d83fc7e7755a6b28e10d470d5
BennettLai2 <bxl190020@tamu.edu>
2022-10-10 14:53:18 -0500

    Added User views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,3 @@
 class SessionsController < ApplicationController
 
-    def create
-        @account = Account.find_by(email: params[:email])
-        if !!@account && @account.authenticate(params[:password])
-            session[:user_id] = @account.email
-            redirect_to accounts_path
-        else 
-            flash[:error] = "Error: Wrong username or password. "
-            
-            redirect_to login_path
-        end
-    end
-
-    def destroy
-        session[:user_id] = ""
-        redirect_to root_path
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :events
   resources :accounts do
     get "meeting"
@@ -7,10 +8,6 @@ Rails.application.routes.draw do
   root 'sessions#login'
 
   get '/admin', to: 'accounts#admin'
-  get '/login', to: 'sessions#login'
-  post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
-  get '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/signup/:id", to: 'events#signup', as: 'signup'
 end
