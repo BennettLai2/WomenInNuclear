aaeaf81a240af2fc4645e9a8e1dbcfaafe2a7362
root <root@localhost>
2022-10-18 02:28:10 +0000

    Changed admin to admin123 to avoid naming conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 User.create!([
-  {first_name: "Admin", last_name: "Person", points: 0, admin: true, email: "admin@gmail.com", password: "123456", encrypted_password: "$2a$12$I19nrxkg0sv0rXm5wSiZfuhy9jOJhrjE44YsIpWetycYp1UVKNehi", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
+  {first_name: "Admin", last_name: "Person", points: 0, admin: true, email: "admin123@gmail.com", password: "123456", encrypted_password: "$2a$12$I19nrxkg0sv0rXm5wSiZfuhy9jOJhrjE44YsIpWetycYp1UVKNehi", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
 ])
