9fe1bf43c8861b44976d0d394d5710ed4dcb3645
root <root@localhost>
2022-10-30 22:55:03 +0000

    Transferred logic from controller to model for meeting in session

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,9 @@ class Event < ApplicationRecord
   validates :points, numericality: {:greater_than_or_equal_to => 0}
   validates_datetime :start_time, on: :create, on_or_after: :today
   validates_datetime :end_time, after: :start_time
-  
+
   def self.with_valid_time(event_id)
-    where("meeting_id = ? AND start_time <= ? AND end_time >= ?", event_id, Time.now, Time.now).first
+      where("meeting_id = ? AND start_time <= ? AND end_time >= ?", event_id, Time.now, Time.now).first rescue nil
   end
 
   private
